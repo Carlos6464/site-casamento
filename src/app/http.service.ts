4596641ceed6b8c631ef01e990a3b9dbc25e310a
Carlos6464 <adrianosodre23@gmail.com>
2025-01-07 20:54:48 -0300

    teste 3

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,5 +1,9 @@
-import { Injectable, inject } from '@angular/core';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {
+  HttpClient,
+  HttpClientModule,
+  HttpHeaders,
+} from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -12,25 +16,51 @@ export class HttpService {
 
   // Método GET
   get<T>(endpoint: string): Observable<T> {
-    return this.http.get<T>(`${this.apiUrl}${endpoint}`);
+    const headers = new HttpHeaders({
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      Pragma: 'no-cache',
+      Expires: '0',
+    });
+    return this.http.get<T>(`${this.apiUrl}${endpoint}`, { headers });
   }
 
   // Método POST
   post<T>(endpoint: string, body: any): Observable<T> {
-    return this.http.post<T>(`${this.apiUrl}${endpoint}`, body);
+    const headers = new HttpHeaders({
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      Pragma: 'no-cache',
+      Expires: '0',
+    });
+    return this.http.post<T>(`${this.apiUrl}${endpoint}`, body, { headers });
   }
 
   // Método PUT
   put<T>(endpoint: string, body: any): Observable<T> {
-    return this.http.put<T>(`${this.apiUrl}${endpoint}`, body);
+    const headers = new HttpHeaders({
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      Pragma: 'no-cache',
+      Expires: '0',
+    });
+    return this.http.put<T>(`${this.apiUrl}${endpoint}`, body, { headers });
   }
 
+  // Método PATCH
   patch<T>(endpoint: string, body: any): Observable<T> {
-    return this.http.patch<T>(`${this.apiUrl}${endpoint}`, body);
+    const headers = new HttpHeaders({
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      Pragma: 'no-cache',
+      Expires: '0',
+    });
+    return this.http.patch<T>(`${this.apiUrl}${endpoint}`, body, { headers });
   }
 
   // Método DELETE
   delete<T>(endpoint: string): Observable<T> {
-    return this.http.delete<T>(`${this.apiUrl}${endpoint}`);
+    const headers = new HttpHeaders({
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      Pragma: 'no-cache',
+      Expires: '0',
+    });
+    return this.http.delete<T>(`${this.apiUrl}${endpoint}`, { headers });
   }
 }
