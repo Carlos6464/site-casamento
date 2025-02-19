0c8bd26699ed16b39f137c560299b97ad3696232
Carlos6464 <adrianosodre23@gmail.com>
2025-02-18 23:06:15 -0300

    ajuste na hora da contagem regrissiva

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,21 +1,21 @@
 import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 
-
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent implements OnInit {
-  targetDate: Date = new Date('2025-05-24T18:00:00'); // Data e hora alvo: 24/05/2025 às 18:00
-  timeLeft: { days: number; hours: number; minutes: number; seconds: number } = {
-    days: 0,
-    hours: 0,
-    minutes: 0,
-    seconds: 0,
-  };
+  targetDate: Date = new Date('2025-05-24T15:30:00'); // Data e hora alvo: 24/05/2025 às 15:30
+  timeLeft: { days: number; hours: number; minutes: number; seconds: number } =
+    {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    };
 
   private intervalId: any;
 
@@ -27,7 +27,6 @@ export class HomeComponent implements OnInit {
         this.startCountdown();
       };
     }
-   
   }
 
   private startCountdown(): void {
@@ -46,16 +45,16 @@ export class HomeComponent implements OnInit {
       // Calcula os valores
       this.timeLeft = {
         days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-        hours: Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+        hours: Math.floor(
+          (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        ),
         minutes: Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)),
         seconds: Math.floor((difference % (1000 * 60)) / 1000),
       };
-
     } else {
       // Quando o tempo termina
       this.timeLeft = { days: 0, hours: 0, minutes: 0, seconds: 0 };
       clearInterval(this.intervalId); // Para o intervalo
     }
   }
-
 }
